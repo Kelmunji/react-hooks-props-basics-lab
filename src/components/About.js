a6b4f0fc39c21501c730f583b0c6e9068edffff1
kelvin Githinji <githinjikelvin74@gmail.com>
2025-04-29 19:31:57 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links"; // Assuming you have a Links component
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* only show <p> if bio exists and is not empty */}
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      {/* add Links component and pass props */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
